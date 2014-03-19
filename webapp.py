9cf32015ab162efacf7e5ffe459310667d5dd2f6
Jacob Drahos <drahos@pyrite-n1.lab.cs.iastate.edu>
2014-03-19 12:37:28 -0500

    Fixed up templating

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html', FIRST='69')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
